864d00e653f86792fdd2ff57b8a33f9a4cd8374d
Nehil Danis <nehild@outlook.com>
2025-07-16 21:15:58 +0200

    feat: changed ransac line fitting to plane fitting

--- src/quiz/ransac/ransac2d.cpp
+++ src/quiz/ransac/ransac2d.cpp
@@ -49,7 +49,7 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr CreateData()
 pcl::PointCloud<pcl::PointXYZ>::Ptr CreateData3D()
 {
 	ProcessPointClouds<pcl::PointXYZ> pointProcessor;
-	return pointProcessor.loadPcd("../../../sensors/data/pcd/simpleHighway.pcd");
+	return pointProcessor.loadPcd("../../../../src/sensors/data/pcd/simpleHighway.pcd");
 }
 
 
@@ -63,6 +63,19 @@ pcl::visualization::PCLVisualizer::Ptr initScene()
   	return viewer;
 }
 
+pcl::PointXYZ cross_product(const pcl::PointXYZ& p1, const pcl::PointXYZ& p2) {
+  pcl::PointXYZ result;
+  result.x = p1.y * p2.z - p1.z * p2.y;
+  result.y = p1.z * p2.x - p1.x * p2.z;
+  result.z = p1.x * p2.y - p1.y * p2.x;
+  return result;
+}
+
+float dot_product(const pcl::PointXYZ& p1, const pcl::PointXYZ& p2) {
+  return  p1.x * p2.x + p1.y * p2.y + p1.z * p2.z;
+}
+
+
 std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
 {
 	std::unordered_set<int> inliersResult;
@@ -73,33 +86,41 @@ std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int ma
 	while(maxIterations--) {
 		std::unordered_set<int> inliers;
 		// Randomly sample subset
-		while(inliers.size() < 2) {
+		while(inliers.size() < 3) {
 			inliers.insert(rand() % cloud->points.size());
 		}
-
-		// fit a line
-		// ax + by + c = 0
-		// n = (a, b) 
 
 		auto itr = inliers.begin();
 		auto x1 = cloud->points[*itr].x;
 		auto y1 = cloud->points[*itr].y;
+		auto z1 = cloud->points[*itr].z;
 		itr++;
 		auto x2 = cloud->points[*itr].x;
 		auto y2 = cloud->points[*itr].y;
+		auto z2 = cloud->points[*itr].z;
+		itr++;
+		auto x3 = cloud->points[*itr].x;
+		auto y3 = cloud->points[*itr].y;
+		auto z3 = cloud->points[*itr].z;
 
-		auto a = (y1 - y2);
-		auto b = (x2 - x1);
-		auto c = (x1*y2 - x2*y1); 
+		auto v1 = pcl::PointXYZ{x2 - x1, y2 - y1, z2 - z1};
+		auto v2 = pcl::PointXYZ{x3 - x1, y3 - y1, z3 - z1};
+
+		auto plane_normal = cross_product(v1, v2);
+
+		auto a = plane_normal.x;
+		auto b = plane_normal.y;
+		auto c = plane_normal.z;
+		auto d = -1 * dot_product(plane_normal, pcl::PointXYZ{x1, y1, z1});
 
 		for(size_t idx = 0; idx < cloud->points.size(); idx++) {
 			const auto& point = cloud->points[idx];
-			auto distace = std::abs(a * point.x + b * point.y + c) / std::sqrt(std::pow(a, 2) + std::pow(b, 2));
+			auto distace = std::abs(a * point.x + b * point.y + c * point.z + d) / std::sqrt(std::pow(a, 2) + std::pow(b, 2) +  std::pow(c, 2));
 			if(distace < distanceTol) {
 				inliers.insert(idx);
 			}
 		}
-		
+
 		if(inliers.size() > max_num_inliers) {
 			max_num_inliers = inliers.size();
 			inliersResult.clear();
@@ -122,7 +143,7 @@ int main ()
 	pcl::visualization::PCLVisualizer::Ptr viewer = initScene();
 
 	// Create data
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData();
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData3D();
 	
 
 	// TODO: Change the max iteration and distance tolerance arguments for Ransac function
