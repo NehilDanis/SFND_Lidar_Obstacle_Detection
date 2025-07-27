6e23648635d67c52cf471d8ff153ed3d195501c6
Nehil Danis <nehild@outlook.com>
2025-07-27 17:49:00 +0200

    feat: added segmentation and clustering

--- src/environment.cpp
+++ src/environment.cpp
@@ -45,11 +45,26 @@ void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer) {
     auto cloud = pc->loadPcd("/Users/nehildanis/Projects/sensorFusion/SFND_Lidar_Obstacle_Detection/src/sensors/data/pcd/data_1/0000000000.pcd");
 
     constexpr float filter_resolution = 0.2;
-    const Eigen::Vector4f min_point{-20, -10, -5, 0.0};
-    const Eigen::Vector4f max_point{20, 10, 5, 0.0};
+    const Eigen::Vector4f min_point{-20, -6, -2, 0.0};
+    const Eigen::Vector4f max_point{20, 6, 5, 0.0};
     pc->FilterCloud(cloud, filter_resolution, min_point, max_point);
-    renderPointCloud(viewer, cloud, "input_cloud");
 
+    auto cloud_pair = pc->SegmentPlane(cloud, 100, 0.2);
+
+    renderPointCloud(viewer, cloud_pair.second, "road", Color{0, 1, 0});
+
+    auto clusters = pc->Clustering(cloud_pair.first, 0.5, 5, 500);
+
+    std::array<Color, 3> colors {Color(1, 1, 0), Color(0, 1, 1), Color(1, 0, 1)};
+
+    auto cluster_index_view = std::views::iota(0U, clusters.size());
+    std::ranges::for_each(cluster_index_view, [&](auto i){
+        auto color_idx = i % colors.size(); // in case more than 3 clusters 
+        // detected some clusters should have same colors. Implemented this way since it is easy
+        renderPointCloud(viewer, clusters[i], "Cluster_" + std::to_string(i), colors[color_idx]);
+        Box box = pc->BoundingBox(clusters[i]);
+        renderBox(viewer, box, i);
+    });
 }
 
 
